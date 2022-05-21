7fc857e9b190e16e233f2315b047204465699fa8
Leiver Campeón <leiverandres04p@gmail.com>
2022-05-21 14:24:10 -0500

    Add simple evaluation sh file

--- configs/evaluation.sh
+++ configs/evaluation.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-./configs/DETReg_fine_tune_1pct_coco.sh --resume exps/DETReg_top30_in100/full_coco_finetune.pth --eval
+python3 main.py --dataset packaging \
+                --resume exps/DETReg_fine_tune_packaging/checkpoint.pth \
+                --eval

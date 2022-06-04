78df443b83a6eff5a68bbb03d97b15c278e20934
Leiver Campeón <leiverandres04p@gmail.com>
2022-06-04 12:31:57 -0500

    Add time taking

--- test_inference.py
+++ test_inference.py
@@ -11,6 +11,7 @@ from util.plot_utils import plot_results
 from matplotlib import pyplot as plt
 import numpy as np
 import random
+import time
 
 args = {'lr': 0.0002, 'max_prop': 30, 'lr_backbone_names': ['backbone.0'], 'lr_backbone': 2e-05, 'lr_linear_proj_names': ['reference_points', 'sampling_offsets'], 'lr_linear_proj_mult': 0.1, 'batch_size': 4, 'weight_decay': 0.0001, 'epochs': 50, 'lr_drop': 40, 'lr_drop_epochs': None, 'clip_max_norm': 0.1, 'sgd': False, 'filter_pct': -1, 'with_box_refine': False, 'two_stage': False, 'strategy': 'topk', 'obj_embedding_head': 'intermediate', 'frozen_weights': None, 'backbone': 'resnet50', 'dilation': False, 'position_embedding': 'sine', 'position_embedding_scale': 6.283185307179586, 'num_feature_levels': 4, 'enc_layers': 6, 'dec_layers': 6, 'dim_feedforward': 1024, 'hidden_dim': 256, 'dropout': 0.1, 'nheads': 8, 'num_queries': 300, 'dec_n_points': 4, 'enc_n_points': 4, 'pretrain': '', 'load_backbone': 'swav', 'masks': False, 'aux_loss': True, 'set_cost_class': 2, 'set_cost_bbox': 5, 'set_cost_giou': 2, 'object_embedding_loss_coeff': 1, 'mask_loss_coef': 1, 'dice_loss_coef': 1, 'cls_loss_coef': 2, 'bbox_loss_coef': 5, 'giou_loss_coef': 2, 'focal_alpha': 0.25, 'dataset_file': 'coco', 'dataset': 'imagenet', 'data_root': 'data', 'coco_panoptic_path': None, 'remove_difficult': False, 'output_dir': '', 'cache_path': 'cache/ilsvrc/ss_box_cache', 'device': 'cuda', 'seed': 42, 'resume': '', 'eval_every': 1, 'start_epoch': 0, 'eval': False, 'viz': False, 'num_workers': 2, 'cache_mode': False, 'object_embedding_loss': False}
 args = Namespace(**args)
@@ -19,15 +20,19 @@ checkpoint = torch.hub.load_state_dict_from_url("https://github.com/amirbar/DETR
 load_msg = model.load_state_dict(checkpoint['model'], strict=False)
 transforms = make_coco_transforms('val')
 
-all_filenames = glob.glob("./data/mask_wearing_dataset/train/*.jpg")
+all_filenames = glob.glob("data/packaging_coco_format/validation/data/*.jpg")
 print(type(all_filenames[0]))
 subset = random.sample(all_filenames, k=5)
+times = []
 for idx, img_path in enumerate(subset):
     # img_url = "https://ak.picdn.net/shutterstock/videos/24611465/thumb/11.jpg"
     # # img_url = "https://media.ktoo.org/2013/10/Brown-Bears.jpg"
     im = Image.open(img_path)
     im_t, _ = transforms(im, None)
+    t_start = time.perf_counter()
     res = model(im_t.unsqueeze(0))
+    t_end = time.perf_counter()
+    times.append(t_end - t_start)
     scores = torch.sigmoid(res['pred_logits'][..., 1])
     pred_boxes = res['pred_boxes']
 
@@ -42,3 +47,4 @@ for idx, img_path in enumerate(subset):
     plot_results(np.array(im), scores_, pred_boxes_, plt.gca(), norm=False)
     plt.axis('off')
     plt.savefig(f"result_{idx}.png")
+mean_inference_time = np.mean(times)
